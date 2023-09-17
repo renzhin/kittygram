c92c2939af430604633da6ecc31d3f84bbc3af0f
Михаил Ренжин <renzhin@gmail.com>
2023-09-17 17:23:52 +0300

    Добавить класс APICat и изменить путь в urls.py

--- cats/views.py
+++ cats/views.py
@@ -1,81 +1,21 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
+
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-@api_view(['GET', 'POST'])
-def cat_list(request):
-    if request.method == 'POST':
-        # Чтобы сериализатор был готов принять список объектов,
-        # в конструктор сериализатора нужно передать
-        # именованный параметр many=True
-        # serializer = CatSerializer(data=request.data, many=True)
+class APICat(APIView):
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
         serializer = CatSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED) 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def cat_update(request, id):
-    cat = Cat.objects.get(id=id)
-    serializer = CatSerializer(cat, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'POST'])  # Применили декоратор и указали разрешённые методы
-def hello(request):
-    # По задумке, в ответ на POST-запрос нужно вернуть JSON с теми данными,
-    # которые получены в запросе.
-    # Для этого в объект Response() передаём словарь request.data.
-    if request.method == 'POST':
-        return Response({'message': 'Получены данные', 'data': request.data})
-
-
-@api_view(['PATCH'])
-def cat_patch(request, id):
-    cat = Cat.objects.get(id=id)
-    serializer = CatSerializer(cat, data=request.data, partial=True)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# @api_view(['GET', 'POST'])
-# def api_posts(request):
-#     if request.method == 'POST':
-#         serializer = PostSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#     posts = Post.objects.all()
-#     serializer = PostSerializer(posts, many=True)
-#     return Response(serializer.data)
-
-
-# @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-# def api_posts_detail(request, pk):
-#     post = Post.objects.get(id=pk)
-#     if request.method == 'PUT' or request.method == 'PATCH':
-#         serializer = PostSerializer(post, data=request.data, partial=True)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#     elif request.method == 'DELETE':
-#         post.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
-#     serializer = PostSerializer(post)
-#     return Response(serializer.data)
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 
-from cats.views import cat_list, hello, cat_update, cat_patch
+from cats.views import APICat
 
 urlpatterns = [
-   path('cats/', cat_list),
-   path('hello/', hello),
-   path('cat_update/<int:id>/', cat_update),
-   path('cat_patch/<int:id>/', cat_patch),
+   path('cats/', APICat.as_view),
 ]
