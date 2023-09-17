db013d8e9b0ffa2bf5ca9311782ecb6cdcd1b8dd
Михаил Ренжин <renzhin@gmail.com>
2023-09-17 14:52:24 +0300

    Добавить view функцию hello и файл requests.http c образцами запросов

--- cats/serializers.py
+++ cats/serializers.py
@@ -6,4 +6,9 @@ from .models import Cat
 class CatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cat
-        fields = '__all__'
+        fields = (
+            'name',
+            'color',
+            'birth_year',
+        )
+        # fields = '__all__'
--- cats/views.py
+++ cats/views.py
@@ -17,3 +17,12 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])  # Применили декоратор и указали разрешённые методы
+def hello(request):
+    # По задумке, в ответ на POST-запрос нужно вернуть JSON с теми данными, 
+    # которые получены в запросе.
+    # Для этого в объект Response() передаём словарь request.data. 
+    if request.method == 'POST':
+        return Response({'message': 'Получены данные', 'data': request.data})
