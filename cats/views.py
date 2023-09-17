85da2e5b242a8d4c1a5823b3ec4d685de090d3b7
Михаил Ренжин <renzhin@gmail.com>
2023-09-17 19:11:40 +0300

    Добавить ModelViewSet

--- cats/views.py
+++ cats/views.py
@@ -1,55 +1,66 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import viewsets 
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-@api_view(['GET', 'POST'])
-def cat_list(request):
-    if request.method == 'POST':
-        # Чтобы сериализатор был готов принять список объектов,
-        # в конструктор сериализатора нужно передать
-        # именованный параметр many=True
-        # serializer = CatSerializer(data=request.data, many=True)
-        serializer = CatSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
 
 
-@api_view(['POST'])
-def cat_update(request, id):
-    cat = Cat.objects.get(id=id)
-    serializer = CatSerializer(cat, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# from rest_framework import status
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+
+# from .models import Cat
+# from .serializers import CatSerializer
 
 
-@api_view(['GET', 'POST'])  # Применили декоратор и указали разрешённые методы
-def hello(request):
-    # По задумке, в ответ на POST-запрос нужно вернуть JSON с теми данными,
-    # которые получены в запросе.
-    # Для этого в объект Response() передаём словарь request.data.
-    if request.method == 'POST':
-        return Response({'message': 'Получены данные', 'data': request.data})
+# @api_view(['GET', 'POST'])
+# def cat_list(request):
+#     if request.method == 'POST':
+#         # Чтобы сериализатор был готов принять список объектов,
+#         # в конструктор сериализатора нужно передать
+#         # именованный параметр many=True
+#         # serializer = CatSerializer(data=request.data, many=True)
+#         serializer = CatSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     cats = Cat.objects.all()
+#     serializer = CatSerializer(cats, many=True)
+#     return Response(serializer.data)
 
 
-@api_view(['PATCH'])
-def cat_patch(request, id):
-    cat = Cat.objects.get(id=id)
-    serializer = CatSerializer(cat, data=request.data, partial=True)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['POST'])
+# def cat_update(request, id):
+#     cat = Cat.objects.get(id=id)
+#     serializer = CatSerializer(cat, data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# @api_view(['GET', 'POST'])  # Применили декоратор и указали разрешённые методы
+# def hello(request):
+#     # По задумке, в ответ на POST-запрос нужно вернуть JSON с теми данными,
+#     # которые получены в запросе.
+#     # Для этого в объект Response() передаём словарь request.data.
+#     if request.method == 'POST':
+#         return Response({'message': 'Получены данные', 'data': request.data})
+
+
+# @api_view(['PATCH'])
+# def cat_patch(request, id):
+#     cat = Cat.objects.get(id=id)
+#     serializer = CatSerializer(cat, data=request.data, partial=True)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # @api_view(['GET', 'POST'])
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,10 +1,31 @@
-from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from cats.views import cat_list, hello, cat_update, cat_patch
+from django.urls import include, path
+
+from cats.views import CatViewSet
+
+# Создаётся роутер
+router = SimpleRouter()
+# Вызываем метод .register с нужными параметрами
+router.register('cats', CatViewSet)
+# В роутере можно зарегистрировать любое количество пар "URL, viewset":
+# например
+# router.register('owners', OwnerViewSet)
+# Но нам это пока не нужно
 
 urlpatterns = [
-   path('cats/', cat_list),
-   path('hello/', hello),
-   path('cat_update/<int:id>/', cat_update),
-   path('cat_patch/<int:id>/', cat_patch),
-]
+    # Все зарегистрированные в router пути доступны в router.urls
+    # Включим их в головной urls.py
+    path('', include(router.urls)),
+] 
+
+# from django.urls import path
+
+# from cats.views import cat_list, hello, cat_update, cat_patch
+
+# urlpatterns = [
+#    path('cats/', cat_list),
+#    path('hello/', hello),
+#    path('cat_update/<int:id>/', cat_update),
+#    path('cat_patch/<int:id>/', cat_patch),
+# ]
